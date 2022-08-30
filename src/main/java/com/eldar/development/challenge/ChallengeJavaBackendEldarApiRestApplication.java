b2225edf60773e88e62639b66e362d5598750fb9
Beniko19 <martin192012@gmail.com>
2022-08-30 17:12:53 -0300

    initial commit.

---
+++ src/main/java/com/eldar/development/challenge/ChallengeJavaBackendEldarApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.eldar.development.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeJavaBackendEldarApiRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeJavaBackendEldarApiRestApplication.class, args);
+    }
+
+}
