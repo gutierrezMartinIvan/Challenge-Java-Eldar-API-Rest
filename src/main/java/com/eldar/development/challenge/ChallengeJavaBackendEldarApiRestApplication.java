250c981bbf7fc521ced2110bdb78817e8ec2cfbe
Beniko19 <martin192012@gmail.com>
2022-08-30 18:13:28 -0300

    updated to ignore the database

--- src/main/java/com/eldar/development/challenge/ChallengeJavaBackendEldarApiRestApplication.java
+++ src/main/java/com/eldar/development/challenge/ChallengeJavaBackendEldarApiRestApplication.java
@@ -2,8 +2,9 @@ package com.eldar.development.challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ChallengeJavaBackendEldarApiRestApplication {
 
     public static void main(String[] args) {
